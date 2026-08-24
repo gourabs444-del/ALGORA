d3927df54baccd1761d00efda1338de8d57fe0b8
gourabs444 <gourabs444@gmail.com>
2026-08-24 20:41:51 +0530

    Convert THANK into matching geometric SelfMadeSystem SVG vector letter glyphs for consistent animated loading state

--- js/inquiry-form.js
+++ js/inquiry-form.js
@@ -108,23 +108,45 @@
               </defs>
             </svg>
 
-            <!-- Animated "THANK YOU" SelfMadeSystem Loader -->
-            <div class="flex flex-col items-center justify-center mb-6 gap-1">
-              <!-- Row 1: "THANK" with Animated Gradient Stroke -->
-              <svg viewBox="0 0 160 32" class="h-7 sm:h-8 w-auto overflow-visible select-none">
-                <text x="50%" y="24" text-anchor="middle" font-family="'Outfit', sans-serif" font-weight="900" font-size="24" letter-spacing="0.22em" fill="none" stroke="url(#sms-b-inj)" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="dash-uiverse">THANK</text>
-              </svg>
+            <!-- Animated "THANK YOU" SelfMadeSystem Full Vector Glyphs -->
+            <div class="flex flex-col items-center justify-center mb-6 gap-2">
+              <!-- Row 1: T - H - A - N - K Vector Glyphs -->
+              <div class="loader-you flex items-center justify-center gap-1">
+                <!-- T -->
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" height="38" width="38" class="inline-block">
+                  <path stroke-linejoin="round" stroke-linecap="round" stroke-width="8" stroke="url(#sms-b-inj)" d="M 8,12 H 56 M 32,12 V 52" class="dash-uiverse" pathLength="360"></path>
+                </svg>
+                <!-- H -->
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" height="38" width="38" class="inline-block">
+                  <path stroke-linejoin="round" stroke-linecap="round" stroke-width="8" stroke="url(#sms-c-inj)" d="M 12,12 V 52 M 12,32 H 52 M 52,12 V 52" class="dash-uiverse" pathLength="360"></path>
+                </svg>
+                <!-- A -->
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" height="38" width="38" class="inline-block">
+                  <path stroke-linejoin="round" stroke-linecap="round" stroke-width="8" stroke="url(#sms-d-inj)" d="M 10,52 L 32,12 L 54,52 M 18,36 H 46" class="dash-uiverse" pathLength="360"></path>
+                </svg>
+                <!-- N -->
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" height="38" width="38" class="inline-block">
+                  <path stroke-linejoin="round" stroke-linecap="round" stroke-width="8" stroke="url(#sms-b-inj)" d="M 14,52 V 12 L 50,52 V 12" class="dash-uiverse" pathLength="360"></path>
+                </svg>
+                <!-- K -->
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" height="38" width="38" class="inline-block">
+                  <path stroke-linejoin="round" stroke-linecap="round" stroke-width="8" stroke="url(#sms-c-inj)" d="M 14,12 V 52 M 48,12 L 14,32 L 48,52" class="dash-uiverse" pathLength="360"></path>
+                </svg>
+              </div>
 
-              <!-- Row 2: "Y - O - U" Animated Vector Stroke Shapes -->
-              <div class="loader-you flex items-center justify-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" height="46" width="46" class="inline-block">
+              <!-- Row 2: Y - O - U Vector Glyphs -->
+              <div class="loader-you flex items-center justify-center gap-1">
+                <!-- Y -->
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" height="42" width="42" class="inline-block">
                   <path stroke-linejoin="round" stroke-linecap="round" stroke-width="8" stroke="url(#sms-b-inj)" d="M 54.722656,3.9726563 A 2.0002,2.0002 0 0 0 54.941406,4 h 5.007813 C 58.955121,17.046124 49.099667,27.677057 36.121094,29.580078 a 2.0002,2.0002 0 0 0 -1.708985,1.978516 V 60 H 29.587891 V 31.558594 A 2.0002,2.0002 0 0 0 27.878906,29.580078 C 14.900333,27.677057 5.0448787,17.046124 4.0507812,4 H 9.28125 c 1.231666,11.63657 10.984383,20.554048 22.6875,20.734375 a 2.0002,2.0002 0 0 0 0.02344,0 c 11.806958,0.04283 21.70649,-9.003371 22.730469,-20.7617187 z" class="dash-uiverse" id="sms-y-inj" pathLength="360"></path>
                 </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" height="46" width="46" class="inline-block">
+                <!-- O -->
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" height="42" width="42" class="inline-block">
                   <path stroke-linejoin="round" stroke-linecap="round" stroke-width="10" stroke="url(#sms-c-inj)" d="M 32 32 m 0 -27 a 27 27 0 1 1 0 54 a 27 27 0 1 1 0 -54" class="spin-uiverse" id="sms-o-inj" pathLength="360"></path>
                 </svg>
-                <div class="w-2" style="width:0.35em;"></div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" height="46" width="46" class="inline-block">
+                <div class="w-1" style="width:0.25em;"></div>
+                <!-- U -->
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64" height="42" width="42" class="inline-block">
                   <path stroke-linejoin="round" stroke-linecap="round" stroke-width="8" stroke="url(#sms-d-inj)" d="M 4,4 h 4.6230469 v 25.919922 c -0.00276,11.916203 9.8364941,21.550422 21.7500001,21.296875 11.616666,-0.240651 21.014356,-9.63894 21.253906,-21.25586 a 2.0002,2.0002 0 0 0 0,-0.04102 V 4 H 56.25 v 25.919922 c 0,14.33873 -11.581192,25.919922 -25.919922,25.919922 a 2.0002,2.0002 0 0 0 -0.0293,0 C 15.812309,56.052941 3.998433,44.409961 4,29.919922 Z" class="dash-uiverse" id="sms-u-inj" pathLength="360"></path>
                 </svg>
               </div>
